50c0f7ee1da3d2f3812a863c1f1f05ad24e1a60b
jarnunes <jarnunesc@gmail.com>
2021-11-02 18:04:50 -0300

    implementar bean para gerenciar gerador de CPF

---
+++ src/main/java/com/ivalidate/view/BeanGenerateCPF.java
@@ -0,0 +1,54 @@
+package com.ivalidate.view;
+
+import com.ivalidate.model.cpf.CPFGenerate;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+
+@Named
+@ManagedBean
+@RequestScoped
+public class BeanGenerateCPF {
+    private boolean started = false;
+    private String cpf;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+
+    public void generate() {
+        this.cpf  =CPFGenerate.generateCPF();
+        this.started = true;
+    }
+
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public void setStarted(boolean started) {
+        this.started = started;
+    }
+
+    public String getIcon() {
+        if (this.started) {
+            if (this.valid) return "pi pi-check";
+            else if (this.cpf == "") return "pi pi-question";
+            else return "pi pi-times";
+        }
+        return "pi pi-question";
+    }
+
+    public String getStyle() {
+        if (this.started) {
+            if (this.valid) return "ui-button-success";
+            else if (this.cpf == "") return "ui-button-secondary";
+            else return "ui-button-danger";
+        }
+        return "ui-button-secondary";
+    }
+}
