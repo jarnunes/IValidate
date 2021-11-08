30d947e7133a85dae026b97d8d67b0a94309ac48
jarnunes <jarnunesc@gmail.com>
2021-11-08 19:17:53 -0300

    Ajustes validação cpf

--- src/test/java/com/ivalidate/model/CpfValidateTest.java
+++ src/test/java/com/ivalidate/model/CpfValidateTest.java
@@ -13,18 +13,13 @@ import com.ivalidate.model.base.CalcBaseDigits;
 public class CpfValidateTest {
     private final String SEQUENCE_CPF = "11111111111";
     private final String INVALID_CPF = "111.111.111-11";
-    private final String VALID_CPF = "524.237.730-56";
+    private final String VALID_CPF = "524.237.730-55";
     private final String CLEAN_CPF = "52423773056";
-    private final String INVALID_DIGIT = "524.237.730-56a";
+    private final String INVALID_DIGIT = "524.237.730-56aa";
 
     @Test
     public void testClearCPF() {
         Assert.assertEquals(CLEAN_CPF, ClearIdentity.clear(VALID_CPF));
-    }
-
-    @Test
-    public void testCalcModule() {
-        Assert.assertEquals(3, CalcBaseDigits.calcModule(162));
     }
 
     @Test
@@ -34,11 +29,17 @@ public class CpfValidateTest {
     }
 
     @Test
+    public void testCalcModule() {
+        Assert.assertEquals(3, CalcBaseDigits.calcModule(162));
+    }
+
+
+
+    @Test
     public void testCalcSecDigit() {
         long SECOND_VALID_SEQ = 1114447773;
         Assert.assertEquals(5, CpfValidate.calcFirstSecDigit(SECOND_VALID_SEQ, 1));
     }
-
 
     @Test
     public void testValidateSequence() {
@@ -59,5 +60,4 @@ public class CpfValidateTest {
     public void testCleanValidCPF() {
         Assert.assertTrue(CpfValidate.validate(CLEAN_CPF));
     }
-
 }
